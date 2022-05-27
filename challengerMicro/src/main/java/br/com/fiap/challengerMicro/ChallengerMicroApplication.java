9c36afa9e7ba850efa3ef9025434016c3cf8f5c2
Yuri-Tokio <yuri@sheild.com.br>
2022-05-27 20:09:41 -0300

    Initial commit

---
+++ challengerMicro/src/main/java/br/com/fiap/challengerMicro/ChallengerMicroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengerMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerMicroApplication.class, args);
+	}
+
+}
